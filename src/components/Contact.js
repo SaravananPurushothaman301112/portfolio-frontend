6a31977118781238891ae21aa6abf6f1e5d9f769
SaravananPurushothaman301112 <saravananraj3011@gmail.com>
2026-02-12 16:41:02 +0530

    updated backend url

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,7 +21,7 @@ function Contact() {
       setLoading(true);
 
       try {
-        const response = await fetch("http://127.0.0.1:8000/contact", {
+        const response = await fetch("https://saravananportfolio.onrender.com/contact", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
